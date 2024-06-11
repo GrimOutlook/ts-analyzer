2323b7e8c75c34e572d704767d749bae679a574e
Dominic Grimaldi <dominic.j.grimaldi@gmail.com>
2024-06-11 17:27:15 -0500

    Working on initial TS reading functionality

--- examples/print_first_packet_payload.rs
+++ examples/print_first_packet_payload.rs
@@ -1,8 +1,3 @@
-# ts-reader
-
-A library used for reading MPEG/Transport Stream files. Mainly created for KLV extraction using [klv-reader](https://github.com/GrimOutlook/klv-reader).
-
-```rust
 extern crate ts_reader;
 
 use std::env;
@@ -23,12 +18,3 @@ fn main() {
     let payload_data = reader.read_next_packet().unwrap().payload().unwrap();
     println!("Payload bytes: {:#?}", payload_data);
 }
-```
-
----
-
-## Reference Material
-
-A sample TS stream with KLV data can be found [here](https://www.arcgis.com/home/item.html?id=55ec6f32d5e342fcbfba376ca2cc409a).
-
-- [Wikipedia: MPEG Transport Stream](https://en.wikipedia.org/wiki/MPEG_transport_stream)
